09a84a4785288464df3057bf81a15283df536f42
Hiroki Nagamine <hirokinagamine_air@HirokinoMacBook-Air.local>
2019-11-02 23:35:17 +0900

    userテーブルの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edet
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
