117f5a5ad28bad557c903699bd41e3e0884edd9d
Hiroki Nagamine <hirokinagamine_air@HirokinoMacBook-Air.local>
2019-11-03 21:55:37 +0900

    userモデルの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_users, only: [:show,:edit,:update]
   def index
     @users = User.all
   end
@@ -12,14 +13,31 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザー登録が完了しました'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'ユーザー登録に失敗しました'
+      render :new
+    end
   end
 
-  def edet
+  def edit
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def set_users
+    @user = User.find(params[:id])
+  end
+
+  def useer_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 end
