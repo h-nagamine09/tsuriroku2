f5d1d900af040c9fe8a9912d2dacffaba1b03eb0
Hiroki Nagamine <hirokinagamine_air@HirokinoMacBook-Air.local>
2019-11-03 23:22:48 +0900

    ユーザー登録フォームの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-before_save {self.email.downcase!}
-  validates :name, presnece: true, length: {maximum :50}
-  validates :email, presence: true, length: {maximum :255},
+  before_save {self.email.downcase!}
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
   　　　　　　　　　　　format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, #メールアドレスの正規表現
   　　　　　　　　　　　uniqueness: {case_sensitive: false} #重複を許さない。大文字小文字を区別しない
 end
