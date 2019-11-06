7d0055947578d571dba53457bfd823799e873589
Hiroki Nagamine <hiroki.nagamine@gmail.com>
2019-11-06 22:52:15 +0900

    メッセージの日本語化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = 'ユーザー登録が完了しました'
+      flash[:success] = 'ユーザー登録が完了しました'
       redirect_to @user
     else
-      flash[:notice] = 'ユーザー登録に失敗しました'
+      flash[:danger] = 'ユーザー登録に失敗しました'
       render :new
     end
   end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = "Tokyo" #タイムゾーンを日本時間に設定
+    config.i18n.default_locale = :ja #i18nで日本語化
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
